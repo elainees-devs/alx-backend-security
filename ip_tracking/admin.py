3ab70c677dcb9606f56b8267b50c4957cb79a3d9
elainees-devs <elainees.devs@gmail.com>
2025-09-02 17:29:52 +0300

    Register IPLog model in Django admin with list display, filters, and search fields

--- ip_tracking/admin.py
+++ ip_tracking/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import IPLog
+
+@admin.register(IPLog)
+class IPLogAdmin(admin.ModelAdmin):
+    list_display = ("ip_address", "path", "method", "timestamp")
+    list_filter = ("method", "timestamp")
+    search_fields = ("ip_address", "path")
