89c05d96b4d47ee8f41e2c7e68e94e5d898f0142
elainees-devs <elainees.devs@gmail.com>
2025-09-02 15:34:06 +0300

    fix import path for ratelimit decorators to django_ratelimit

--- ip_tracking/views.py
+++ ip_tracking/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django_ratelimit.decorators import ratelimit
 
-# Create your views here.
+from django.http import JsonResponse
+
+@ratelimit(key='ip', rate='10/m', method='POST', block=True)
+def login_view(request):
+    return JsonResponse({"message": "Login endpoint accessed"})
