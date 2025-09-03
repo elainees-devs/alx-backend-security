6c09f294fb3c7685316ef04bbe2deece8a39e284
elainees-devs <elainees.devs@gmail.com>
2025-09-03 08:31:31 +0300

    Update admin registration from IPLog to RequestLog

--- ip_tracking/admin.py
+++ ip_tracking/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
+from .models import RequestLog
 
-# Register your models here.
-from django.contrib import admin
-from .models import IPLog
-
-@admin.register(IPLog)
-class IPLogAdmin(admin.ModelAdmin):
+@admin.register(RequestLog)
+class RequestLogAdmin(admin.ModelAdmin):
     list_display = ("ip_address", "path", "method", "timestamp")
     list_filter = ("method", "timestamp")
     search_fields = ("ip_address", "path")
